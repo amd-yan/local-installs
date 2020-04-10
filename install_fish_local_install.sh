8f7adc413307a69d981af8f3e3aa2d91ff9dec68
Yan <shaohyan@atlhdp30-endv01.amd.com>
2020-04-10 19:48:26 -0400

    Init Comit

---
+++ install_fish_local_install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+ 
+# Script for installing Fish Shell on systems without root access.
+# Fish Shell will be installed in $HOME/local/bin.
+# It's assumed that wget and a C/C++ compiler are installed.
+ 
+# exit on error
+set -e
+ 
+FISH_SHELL_VERSION=3.0.2
+ 
+# create our directories
+mkdir -p $HOME/local $HOME/fish_shell_tmp
+cd $HOME/fish_shell_tmp
+ 
+# download source files for Fish Shell
+wget http://github.com/fish-shell/fish-shell/releases/download/${FISH_SHELL_VERSION}/fish-${FISH_SHELL_VERSION}.tar.gz
+ 
+# extract files, configure, and compile
+
+tar xvzf fish-${FISH_SHELL_VERSION}.tar.gz
+cd fish-${FISH_SHELL_VERSION}
+./configure --prefix=$HOME/local --disable-shared
+
+make
+make install
